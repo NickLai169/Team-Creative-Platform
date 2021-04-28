9b49196f9daa0e77d47b8127c48d8eb7535bdd79
NickLai169 <nick-1112010@hotmail.co.nz>
2021-04-27 20:50:54 -0700

    beginning implementation of recruiter review page

---
+++ nestor/src/components/Recruiter_review_applicants.jsx
@@ -0,0 +1,15 @@
+import React, { useState } from 'react';
+import styled from 'styled-components';
+import { VerticalTimeline, VerticalTimelineElement }  from 'react-vertical-timeline-component';
+import 'react-vertical-timeline-component/style.min.css';
+import * as BsIcons from 'react-icons/bs';
+
+const Recruiter_review_applicants = () => {
+    return (
+        <>
+            <h1>Your Position Listings</h1>
+        </>
+    );
+}
+
+export default Recruiter_review_applicants;
