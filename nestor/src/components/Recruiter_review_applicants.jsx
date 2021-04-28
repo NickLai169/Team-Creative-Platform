0c35e24518c2742eaab0800de417c020c8efdcb7
NickLai169 <nick-1112010@hotmail.co.nz>
2021-04-27 21:00:44 -0700

    update

--- nestor/src/components/Recruiter_review_applicants.jsx
+++ nestor/src/components/Recruiter_review_applicants.jsx
@@ -1,4 +1,5 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import { Container, Row, Col } from 'reactstrap';
 import styled from 'styled-components';
 import { VerticalTimeline, VerticalTimelineElement }  from 'react-vertical-timeline-component';
 import 'react-vertical-timeline-component/style.min.css';
